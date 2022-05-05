c09f6a5d949436d830baa84fec5002b0131ccdc9
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-05 20:39:13 -0300

    Update contato.js

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,54 +1,13 @@
-var ID_CONTATO_INC = 3;
-
-var contatos = [
-    { _id: 1, nome: 'Fabio Teixeira', email: 'fabio.teixeira@ifsp.edu.br' },
-    { _id: 2, nome: 'Fabiano Teixeira', email: 'fabiano.teixeira@ifsp.edu.br' },
-    { _id: 3, nome: 'Melissa Teixeira', email: 'melissa.teixeira@ifsp.edu.br' }
-]
-
-module.exports = function() {
+module.exports = function(app) {
+    var Contato = app.models.contato;
     var controller = {};
     controller.listaContatos = function(req, res) {
-        res.json(contatos);
+        Contato.find().exec().then(
+            function(contatos) {
+                res.json(contatos);
+            },
+            function(erro) {
+                console.error(erro)
+                res.status(500).json(erro);
+            });
     };
-    controller.obtemContato = function(req, res) {
-        console.log('Selecionou o contato: ' + req.params.id);
-        var idContato = req.params.id;
-        var contato = contatos.filter(function(contato) {
-            return contato._id == idContato;
-        })[0];
-        contato ? res.json(contato) : res.status(404).sendStatus('Contato não encontrado!');
-    };
-    controller.removeContato = function(req, res) {
-        var idContato = req.params.id;
-        contatos = contatos.filter(function(contato) {
-            return contato._id != idContato;
-        });
-        res.sendStatus(204).end();
-    };
-
-    controller.salvaContato = function(req, res) {
-        var contato = req.body;
-        contato = contato._id ? atualiza(contato) : adiciona(contato);
-        res.json(contato);
-    };
-
-    function adiciona(contatoNovo) {
-        contatoNovo._id = ++ID_CONTATO_INC;;
-        contatos.push(contatoNovo);
-        return contatoNovo;
-    }
-
-    function atualiza(contatoAlterar) {
-        contatos = contatos.map(function(contato) {
-            if (contato._id == contatoAlterar._id) {
-                contato = contatoAlterar;
-            }
-            return contato;
-        });
-
-        return contatoAlterar;
-    }
-
-    return controller;
-};
