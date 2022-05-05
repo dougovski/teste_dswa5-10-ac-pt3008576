4bd25088f97fad6b0c19ab304da787cda1151a91
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-05 20:20:14 -0300

    Create server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express')();
-const url = 'mongodb+srv://dougovski:1409@clusterdhaps.rzyac.mongodb.net/ifsp?retryWrites=true&w=majority';
+const url = 'mongodb://dougovski:1409@clusterdhaps-shard-00-00.rzyac.mongodb.net:27017,clusterdhaps-shard-00-01.rzyac.mongodb.net:27017,clusterdhaps-shard-00-02.rzyac.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-z0ehqd-shard-0&authSource=admin&retryWrites=true&w=majority';
 require('./config/database.js')(url);
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));
