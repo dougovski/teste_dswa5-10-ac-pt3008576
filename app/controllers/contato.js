dc36cd1297c48c168e7ade8cacf06f30c45b2255
Douglas <88936801+dougovski@users.noreply.github.com>
2022-05-05 20:44:34 -0300

    Update contato.js

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -24,6 +24,41 @@ module.exports = function(app) {
     res.status(404).json(erro)
      });
     };
+    
+    controller.removeContato = function(req, res) {
+        var _id = req.params.id;
+        Contato.deleteOne({ "_id": _id }).exec().then(
+            function() {
+                res.end();
+            },
+            function(erro) {
+                return console.error(erro);
+            });
+          };
+
+    controller.salvaContato = function(req, res) {
+        var _id = req.body._id;
+        if (_id) {
+            Contato.findByIdAndUpdate(_id, req.body).exec().then(
+                function(contato) {
+                    res.json(contato);
+                },
+                function(erro) {
+                    console.error(erro)
+                    res.status(500).json(erro);
+                });
+        } else {
+            Contato.create(req.body).then(
+                function(contato) {
+                    res.status(201).json(contato);
+                },
+                function(erro) {
+                    console.log(erro);
+                    res.status(500).json(erro);
+                });
+        }
+    };
+
 
     controller.obtemContato = function(req, res) {};
     controller.removeContato = function(req, res) {};
